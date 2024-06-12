054cb245035ba5760a9e4e7eb9887a313b0290b0
Sersid <sersoned@gmail.com>
2024-06-12 23:43:00 +0300

    Fix code in psr12 standard

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,26 +1,20 @@
 <?php
+
 declare(strict_types=1);
 
 use PhpCsFixer\Config;
-use Samson\PhpCsFixer\Fixer\PhpTag\WhitespaceAfterOpeningShortTagFixer;
 use Symfony\Component\Finder\Finder;
 
 $finder = (new Finder())
-    ->in(__DIR__ . '/example')
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
     ->files()
     ->name('/\.php$/')
 ;
 
 return (new Config())
-    ->registerCustomFixers([
-        new WhitespaceAfterOpeningShortTagFixer(),
-    ])
-    ->setRules([
-        // 'linebreak_after_opening_tag' => true,
-        'Samson/white_space_after_opening_short_tag' => true,
-        // 'no_closing_tag' => true,
-        // 'lowercase_cast' => true,
-        'full_opening_tag' => true,
-    ])
     ->setFinder($finder)
+    ->setRules([
+        '@PSR12' => true,
+    ])
 ;
--- src/Fixer/PhpTag/WhitespaceAfterOpeningShortTagFixer.php
+++ src/Fixer/PhpTag/WhitespaceAfterOpeningShortTagFixer.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Samson\PhpCsFixer\Fixer\PhpTag;
